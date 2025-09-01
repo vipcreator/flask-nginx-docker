eab1256b2431d87113c1e3c770ac0ec12bf357db
vipcreator devops <vipoverinov@gmail.com>
2025-09-01 13:18:02 +0000

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Hello from Flask! Uptime: {datetime.utcnow()}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
